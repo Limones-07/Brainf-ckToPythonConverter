94c8e42149a886df37fb6366ec23df5b886c4ec4
Limones-07 <07limones@gmail.com>
2023-02-22 12:30:19 -0300

    Now it actually converts the code

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import sys
 import os
-import os.path
-from functions import *
+from pathlib import Path
+from utils import *
 
 def main(code_path):
     # Loads the code and the array on the memory
     code, array = open_code(code_path)
-    # print(code)
-    # print(array)
-    
-    # Create and opens the output file
+
+    # Creates and opens the output file
     try:
         os.mkdir('out')
     except FileExistsError:
         pass
-    os.chdir('out')
-    output = open('code.py', 'w+')
+    output = open(Path('out/code.py'), 'w+')
 
+    # Initializes the required functions, the array and the pointer to the output file
+    with open('basic_functions.py', 'rt') as f:
+        output.write(f.read())
+    output.write(f'array = {array}\n')
+    output.write(f'pointer = 0\n')
+
+    # Initializes the identation variable
+    identation = ''
+
+    # Converts
+    for character in code:
+        match character:
+            case '>':
+                output.write(f'{identation}pointer = move_right_pointer(array, pointer)\n')
+            case '<':
+                output.write(f'{identation}pointer = move_left_pointer(array, pointer)\n')
+            case '+':
+                output.write(f'{identation}increase_pointer(array, pointer)\n')
+            case '-':
+                output.write(f'{identation}decrease_pointer(array, pointer)\n')
+            case '.':
+                output.write(f'{identation}print_pointer(array, pointer)\n')
+            case ',':
+                output.write(f'{identation}input_pointer(array, pointer)\n')
+            case '[':
+                identation = insert_loop_open(output, identation)
+            case ']':
+                identation = insert_loop_close(identation)
+    
+    # Closes the file
     output.close()
 
 
 if __name__ == "__main__":
     # Check file existance
     try:
-        if os.path.isfile(sys.argv[1]):
+        if Path(sys.argv[1]).is_file():
             main(sys.argv[1])
+            print('It\'s done! :)')
         else:
             print('The first argument must be the path to the .bf file. ')
             exit(1)
-    except IndexError as e:
-        print('You must provide the .bf file\'s path on the first argument. ')
+    except IndexError:
+        print('You must specify the path to the .bf file. ')
         exit(1)
